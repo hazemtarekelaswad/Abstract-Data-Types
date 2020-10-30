699e4fd1258a7f656f3c284775f4fe2d83b26a92
hazemtarekelaswad <hazem.elaswad00@eng-st.cu.edu.eg>
2020-10-30 03:00:19 +0200

    implement all of the functions in ArrayBag

--- Bag/Bag/ArrayBag.h
+++ Bag/Bag/ArrayBag.h
@@ -4,7 +4,7 @@
 #include "Bag.h"
 
 template<typename T, int Cap>
-class ArrayBag : Bag<T> {
+class ArrayBag : public Bag<T> {
 private:
 	T m_bag[Cap];
 	int size;
@@ -12,31 +12,62 @@ private:
 public:
 	ArrayBag() : size(0) {}
 
-	int GetSize() const {
+	int GetSize() const override {
 		return size;
 	}
 
-	bool IsEmpty() const {
+	bool IsEmpty() const override {
 		return size == 0;
 	}
 
-	bool Add(const T& item) {
+	bool Add(const T& item) override {
 		if (size == Cap)
 			return false;
 		m_bag[size++] = item;
 		return true;
 	}
 
-	bool Remove(const T& item) {
-		
+	bool Remove(const T& item) override {
+		for (int i = 0; i < size; ++i) {
+			if (m_bag[i] == item) {
+				m_bag[i] = m_bag[size - 1];
+				--size;
+				return true;
+			}
+		}
+		return false;
 	}
 
-	void Clear() {
+	void Clear() override {
 		size = 0;
 	}
 
-	int GetFrequencyOf(const T& item) const {}
-	bool Contains(const T& item) const {}
-	std::vector<T> ToVector() const {}
+	int GetFrequencyOf(const T& item) const override {
+		int count = 0;
+		for (int i = 0; i < size; ++i)
+			if (m_bag[i] == item)
+				++count;
+		return count;
+	}
+
+	bool Contains(const T& item) const override {
+		for (int i = 0; i < size; ++i)
+			if (m_bag[i] == item)
+				return true;
+		return false;
+	}
+
+	friend std::ostream& operator<<(std::ostream& out, const ArrayBag& bag) {
+		for (int i = 0; i < bag.size; ++i)
+			out << bag.m_bag[i] << ' ';
+		return out;
+	}
+
+	std::vector<T> ToVector() const override {
+		std::vector<T> vec(size);
+		for (int i = 0; i < size; ++i)
+			vec.push_back(m_bag[i]);
+		return vec;
+	}
 };
 
