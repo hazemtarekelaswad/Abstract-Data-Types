8303cb3e249887f592dd2932495aa627ac91da45
hazemtarekelaswad <hazem.elaswad00@eng-st.cu.edu.eg>
2020-10-30 03:13:30 +0200

    add Node class

--- Bag/Bag/ListBag.h
+++ Bag/Bag/ListBag.h
@@ -1,7 +1,76 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include "Node.h"
+#include "Bag.h"
 
+template<typename T>
+class LinkedBag : public Bag<T> {
+private:
+	Node<T>* m_head;
+	int size;
+
+public:
+	LinkedBag() : m_head(nullptr), size(0) {}
+
+	int GetSize() const override {
+		return size;
+	}
+
+	bool IsEmpty() const override {
+		return size == 0;
+	}
+
+	bool Add(const T& item) override {
+		if (size == Cap)
+			return false;
+		m_bag[size++] = item;
+		return true;
+	}
+
+	bool Remove(const T& item) override {
+		for (int i = 0; i < size; ++i) {
+			if (m_bag[i] == item) {
+				m_bag[i] = m_bag[size - 1];
+				--size;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	void Clear() override {
+		size = 0;
+	}
+
+	int GetFrequencyOf(const T& item) const override {
+		int count = 0;
+		for (int i = 0; i < size; ++i)
+			if (m_bag[i] == item)
+				++count;
+		return count;
+	}
+
+	bool Contains(const T& item) const override {
+		for (int i = 0; i < size; ++i)
+			if (m_bag[i] == item)
+				return true;
+		return false;
+	}
+
+	friend std::ostream& operator<<(std::ostream& out, const ArrayBag& bag) {
+		for (int i = 0; i < bag.size; ++i)
+			out << bag.m_bag[i] << ' ';
+		return out;
+	}
+
+	std::vector<T> ToVector() const override {
+		std::vector<T> vec(size);
+		for (int i = 0; i < size; ++i)
+			vec.push_back(m_bag[i]);
+		return vec;
+	}
+};
 
 
 
---
+++ Bag/Bag/Node.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+
+template<typename T>
+class Node {
+private:
+	T data;
+	Node* next;
+
+public:
+	Node() : next(nullptr) {}
+
+	Node(const T& data) : data(data), next(nullptr) {}
+
+	Node(const T& data, Node* node) : data(data), next(node) {}
+
+	void SetData(const T& data) {
+		this->data = data;
+	}
+
+	void SetNext(Node* node) {
+		next = node;
+	}
+
+	T GetData() const {
+		return data;
+	}
+
+	Node* GetNext() const {
+		return next;
+	}
+};
+
+
