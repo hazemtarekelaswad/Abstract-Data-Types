eb429ae71dc338c567d36f7a50eed723c6c8bffd
hazemtarekelaswad <hazem.elaswad00@eng-st.cu.edu.eg>
2020-11-14 02:09:27 +0200

    add the interface operations

--- List/List/List.h
+++ List/List/List.h
@@ -1,5 +1,17 @@
 #pragma once
 #include <iostream>
-using namespace std;
+
+template<typename T>
+class List {
+public:
+	virtual bool IsEmpty() const = 0;
+	virtual int GetSize() const = 0;
+	virtual void Clear() = 0;
+	virtual bool Insert(int index) = 0;
+	virtual bool Remove(int index) = 0;
+	virtual bool Remove(const T& item) = 0;
+	virtual void Replace(int index, const T& item) = 0;
+	virtual T GetItem(int index) const = 0;
+};
 
 
