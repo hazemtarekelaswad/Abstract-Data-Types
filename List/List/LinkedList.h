a1ee251cc245ff5fd9400b4709d35c517eba816d
hazemtarekelaswad <hazem.elaswad00@eng-st.cu.edu.eg>
2020-11-14 13:41:37 +0200

    add basic functions to linkedList

--- List/List/LinkedList.h
+++ List/List/LinkedList.h
@@ -10,15 +10,36 @@ private:
 	int size;
 
 public:
-	LinkedList() : size(0) {}
-
+	LinkedList() : size(0), m_head(nullptr) {}
+	LinkedList(const LinkedList& lst) {
+	}
+	LinkedList& operator=(const LinkedList& lst) {
+	}
 	bool IsEmpty() const override {
+		return (m_head == nullptr);
 	}
 	int GetSize() const override {
+		return size;
 	}
 	void Clear() override {
 	}
 	bool Insert(int index, const T& item) override {
+		if (index < 0 || index > size)
+			return false;
+
+		if (!index) {
+			Node<T>* newNode = new Node<T>(item, m_head);
+			m_head = newNode;
+		}
+		else {
+			Node<T>* temp = m_head;
+			while (--index)
+				temp = temp->GetNext();
+			Node<T>* newNode = new Node<T>(item, temp->GetNext());
+			temp->SetNext(newNode);
+		}
+		++size;
+		return true;
 	}
 	bool RemoveAt(int index) override {
 	}
@@ -27,5 +48,9 @@ public:
 	void Replace(int index, const T& item) override {
 	}
 	T GetItem(int index) const override {
+	}
+
+	~LinkedList() {
+		Clear();
 	}
 };
