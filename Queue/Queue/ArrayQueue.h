18d0cc204a027dc52f05874a0d85dbdcc50d5c1d
hazemtarekelaswad <hazem.elaswad00@eng-st.cu.edu.eg>
2020-11-24 12:19:58 +0200

    add function prototypes

--- Queue/Queue/ArrayQueue.h
+++ Queue/Queue/ArrayQueue.h
@@ -1,1 +1,30 @@
 #pragma once
+#include <iostream>
+#include "Queue.h"
+
+template<typename T>
+class ArrayQueue : public Queue<T> {
+private:
+public:
+	bool IsEmpty() const {
+
+	}
+	bool IsFull() const {
+
+	}
+	bool Enqueue(const T& item) {
+
+	}
+	bool Dequeue(const T& item) {
+
+	}
+	T Front() const {
+
+	}
+	T Back() const {
+
+	}
+	void Print() const {
+
+	}
+};
--- Queue/Queue/LinkedQueue.h
+++ Queue/Queue/LinkedQueue.h
@@ -1,1 +1,31 @@
 #pragma once
+#include <iostream>
+#include "Node.h"
+#include "Queue.h"
+
+template<typename T>
+class LinkedQueue : public Queue<T> {
+private:
+public:
+	bool IsEmpty() const {
+
+	}
+	bool IsFull() const {
+
+	}
+	bool Enqueue(const T& item) {
+
+	}
+	bool Dequeue(const T& item) {
+
+	}
+	T Front() const {
+
+	}
+	T Back() const {
+
+	}
+	void Print() const {
+
+	}
+};
