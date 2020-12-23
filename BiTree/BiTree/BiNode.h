66582cc327e02fb190964f9beea62ba66c9af42c
hazemtarekelaswad <hazem.elaswad00@eng-st.cu.edu.eg>
2020-12-23 12:38:31 +0200

    implememnt some functions of LinkedBiTree

--- BiTree/BiTree/BiNode.h
+++ BiTree/BiTree/BiNode.h
@@ -1,1 +1,45 @@
 #pragma once
+#include <iostream>
+
+template<typename T>
+class BiNode{
+private:
+	T data;
+	BiNode* left;
+	BiNode* right;
+
+public:
+	BiNode() : left(nullptr), right(nullptr) {}
+	BiNode(const T& data) : data(data), left(nullptr), right(nullptr) {}
+	BiNode(const T& data, BiNode* left) : data(data), left(left), right(nullptr) {}
+	BiNode(const T& data, BiNode* left, BiNode* right) : data(data), left(left), right(right) {}
+
+	void SetData(const T& data) {
+		this->data = data;
+	}
+
+	void SetLeft(BiNode* node) {
+		left = node;
+	}
+
+	void SetRight(BiNode* node) {
+		right = node;
+	}
+
+	void SetChildren(BiNode* lft, BiNode* ryt) {
+		left = lft;
+		right = ryt;
+	}
+
+	T GetData() const {
+		return data;
+	}
+
+	BiNode* GetLeft() const {
+		return left;
+	}
+
+	BiNode* GetRight() const {
+		return right;
+	}
+};
--- BiTree/BiTree/LinkedBiTree.h
+++ BiTree/BiTree/LinkedBiTree.h
@@ -1,1 +1,43 @@
 #pragma once
+#include <iostream>
+#include <cassert>
+#include "BiNode.h"
+#include "BiTree.h"
+
+template<typename T>
+class LinkedBiTree : public BiTree<T> {
+private:
+	BiNode<T>* root;
+	int count;
+
+public:
+	LinkedBiTree() : root(nullptr), count(0) {}
+
+	LinkedBiTree(const T& item) : count(1) {
+		root = new BiNode<T>(item);
+	}
+
+	LinkedBiTree(const LinkedBiTree<T>& biTree) {}
+
+	T GetRoot() const {
+		assert(!IsEmpty());
+		return root->GetData();
+	}
+	int GetSize() const {
+		return count;
+	}
+	bool IsEmpty() const {
+
+	}
+	int GetHeight() const {
+
+	}
+	void Clear() {
+
+	}
+	void Print() const {
+
+	}
+
+	~LinkedBiTree() {}
+};
