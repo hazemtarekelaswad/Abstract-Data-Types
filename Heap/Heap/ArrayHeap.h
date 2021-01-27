0578f917e1ccf13c97e0fec11d8fbc74eff76c70
hazemtarekelaswad <hazem.elaswad00@eng-st.cu.edu.eg>
2021-01-27 03:21:30 +0200

    Implement the Array Heap class

--- Heap/Heap/ArrayHeap.h
+++ Heap/Heap/ArrayHeap.h
@@ -1,1 +1,118 @@
 #pragma once
+#include <iostream>
+#include <utility>
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include "Heap.h"
+
+template<typename T, int capacity>
+class ArrayHeap : public Heap<T> {
+private:
+	T data[capacity]{};
+	int size;
+
+private:
+
+	int GetLeftChildIndex(int nodeIndex) const {
+		return 2 * nodeIndex + 1;
+	}
+	int GetRightChildIndex(int nodeIndex) const {
+		return 2 * nodeIndex + 2;
+	}
+	int GetParentIndex(int nodeIndex) const {
+		return (nodeIndex - 1) / 2;
+	}
+	bool IsLeaf(int nodeIndex) const {
+		return GetLeftChildIndex(nodeIndex) >= size;
+	}
+	void ReheapUp() {
+		int newIndex = size - 1;
+		while (newIndex && data[newIndex] > data[GetParentIndex(newIndex)]) {
+			std::swap(data[newIndex], data[GetParentIndex(newIndex)]);
+			newIndex = GetParentIndex(newIndex);
+		}
+	}
+
+	void ReheapDown(int root) {
+		if (IsLeaf(root))
+			return;
+
+		int firstChildIndex = GetLeftChildIndex(root);
+		int secChildIndex = GetRightChildIndex(root);
+		if (data[firstChildIndex] > data[secChildIndex] && data[root] < data[firstChildIndex]) {
+			std::swap(data[firstChildIndex], data[root]);
+			ReheapDown(firstChildIndex);
+		}
+		else if (data[secChildIndex] > data[firstChildIndex] && data[root] < data[secChildIndex]) {
+			std::swap(data[secChildIndex], data[root]);
+			ReheapDown(secChildIndex);
+		}
+	}
+
+	/*void ReheapDown() {
+		int root = 0;
+		while (root < size) {
+			int firstChildIndex = GetLeftChildIndex(root);
+			int secChildIndex = GetRightChildIndex(root);
+			if (firstChildIndex == size - 1 && data[root] < data[firstChildIndex])
+				std::swap(data[firstChildIndex], data[root]);
+			else if (firstChildIndex < size - 1 && secChildIndex < size) {
+				if (data[firstChildIndex] > data[secChildIndex] && data[root] < data[firstChildIndex]) {
+					std::swap(data[firstChildIndex], data[root]);
+					root = firstChildIndex;
+				}
+				else if (data[secChildIndex] > data[firstChildIndex] && data[root] < data[secChildIndex]) {
+					std::swap(data[secChildIndex], data[root]);
+					root = secChildIndex;
+				}
+			}
+			else
+				return;
+		}
+	}*/
+
+
+public:
+	ArrayHeap() : size(0) {}
+	bool IsEmpty() const override {
+		return size == 0;
+	}
+	int Size() const override {
+		return size;
+	}
+	int GetHeight() const override {
+		return std::ceil(log2(size + 1)) - 1;
+	}
+	T Peek() const override {
+		assert(!IsEmpty());
+		return data[0];
+	}
+	bool Add(const T& item) override {
+		if (size == capacity)
+			return false;
+		data[size++] = item;
+		ReheapUp();
+		return true;
+	}
+	bool Remove() override {
+		if (IsEmpty())
+			return false;
+		data[0] = data[size - 1];
+		--size;
+		ReheapDown(0);
+		return true;
+	}
+	void Clear() override {
+		size = 0;
+	}
+
+	void Print() const override {
+		for (int i = 0; i < size; ++i)
+			std::cout << data[i] << ' ';
+		std::cout << '\n';
+	}
+
+	virtual ~ArrayHeap() {}
+
+};
--- Heap/Heap/Heap.h
+++ Heap/Heap/Heap.h
@@ -1,11 +1,13 @@
 #pragma once
 template<typename T>
 class Heap {
-	virtual void IsEmpty() const = 0;
+	virtual bool IsEmpty() const = 0;
 	virtual int Size() const = 0;
-	virtual int GetHeight() const = 0;
+	virtual int GetHeight() const = 0;	// starting with the 0th level
 	virtual T Peek() const = 0;
 	virtual bool Add(const T& item) = 0;
 	virtual bool Remove() = 0;
 	virtual void Clear() = 0;
+
+	virtual void Print() const = 0;
 };
