61c9b1a0f217d3b147bac24b2af4815551239013
hazemtarekelaswad <hazem.elaswad00@eng-st.cu.edu.eg>
2021-01-28 21:57:16 +0200

    Add Dictionary Interface functions

--- Dictionary/Dictionary/Dictionary.h
+++ Dictionary/Dictionary/Dictionary.h
@@ -3,5 +3,11 @@
 template<typename K, typename T>
 class Dictionary {
 public:
-
+	virtual bool IsEmpty() const = 0;
+	virtual int GetSize() const = 0;
+	virtual void Clear() const = 0;
+	virtual bool Add(const K& key, const T& item) = 0;
+	virtual bool Remove(const K& key) = 0;
+	virtual T GetItem(const K& key) = 0;
+	virtual bool Contains(const const K& key) = 0;
 };
